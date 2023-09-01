a598907cacb9da1edb817c8f893f2cf84df2cd53
Mohammed Al Mitwalli <mohammed.almitwalli@gmail.com>
2023-09-01 03:17:42 -0700

    Complete assignment

--- server/app.py
+++ server/app.py
@@ -19,15 +19,22 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter_by(id=id).first()
+    return f'<ul>ID: {id}</ul><ul>Name: {animal.name}</ul><ul>Zookeeper: {animal.zookeeper.name}</ul><ul>Species: {animal.species}</ul><ul>Enclosure: {animal.enclosure.environment}</ul>'
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter_by(id=id).first()
+    animal_list = [f'<ul>Animal: {animal.name}</ul>' for animal in zookeeper.animals]
+    formatted_list = '\n'.join(animal_list)
+    return f'<ul>ID: {id}</ul><ul>Name: {zookeeper.name}</ul><ul>Birthday: {zookeeper.birthday}</ul>{formatted_list}'
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter_by(id=id).first()
+    animal_list = [f'<ul>Animal: {animal.name}</ul>' for animal in enclosure.animals]
+    formatted_list = '\n'.join(animal_list)
+    return f'<ul>ID: {id}</ul><ul>Environment: {enclosure.environment}</ul><ul>Open to Visitors: {enclosure.open_to_visitors}</ul>{formatted_list}'
 
 
 if __name__ == '__main__':
